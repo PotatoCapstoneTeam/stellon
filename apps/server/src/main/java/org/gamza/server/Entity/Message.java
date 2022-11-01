d3cadbb2673d08ec1356e259bae5bbbe49839c94
cheomuk <pkw9815@naver.com>
2022-11-01 21:41:52 +0900

    Revise EC2

--- apps/server/src/main/java/org/gamza/server/Entity/Message.java
+++ apps/server/src/main/java/org/gamza/server/Entity/Message.java
@@ -3,17 +3,17 @@ package org.gamza.server.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-public class Message {
+  @Getter
+  @Setter
+  public class Message {
 
-  // 메시지 타입: 입장, 대기방, 인 게임, 퇴장
-  public enum MessageType {
-    JOIN, ROOM, START, PLAY, EXIT
+    // 메시지 타입: 입장, 대기방, 인 게임, 퇴장
+    public enum MessageType {
+      JOIN, ROOM, START, PLAY, EXIT
+    }
+
+    private MessageType type;   // 메시지 타입
+    private UserInfo userInfo;
+    private String message;
+    private GameRoom gameRoom;
   }
-
-  private MessageType type;   // 메시지 타입
-  private UserInfo userInfo;
-  private String message;
-  private GameRoom gameRoom;
-}
