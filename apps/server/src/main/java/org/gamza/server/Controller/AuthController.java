c1fbd412db20e0711ad1b62a36fbb6c887f1b76f
JOE1534 <king01286@naver.com>
2022-11-02 15:04:01 +0900

    Add Cors

--- apps/server/src/main/java/org/gamza/server/Controller/AuthController.java
+++ apps/server/src/main/java/org/gamza/server/Controller/AuthController.java
@@ -6,14 +6,12 @@ import org.gamza.server.Dto.UserDto.UserJoinDto;
 import org.gamza.server.Dto.UserDto.UserLoginDto;
 import org.gamza.server.Service.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@CrossOrigin(origins = "localhost:4200")
 public class AuthController {
 
   private final AuthService authService;
