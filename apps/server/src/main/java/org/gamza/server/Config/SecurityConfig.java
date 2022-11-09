065c1cc879073e22f245b00c45da61e8a18439cd
JOE1534 <king01286@naver.com>
2022-11-09 15:26:16 +0900

    Cors Error Fix #2

--- apps/server/src/main/java/org/gamza/server/Config/SecurityConfig.java
+++ apps/server/src/main/java/org/gamza/server/Config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.gamza.server.Error.SecurityErrorHandler.AuthenticationEntryPointHandl
 import org.gamza.server.Error.SecurityErrorHandler.WebAccessDeniedHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,6 +35,7 @@ public class SecurityConfig {
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and()
       .authorizeRequests()
+      .antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
       .antMatchers("/ws-stomp/**").permitAll()
       .antMatchers("/auth/login", "/auth/join", "/auth/reissue").permitAll()
       .antMatchers("/ws-chat/**").permitAll()
