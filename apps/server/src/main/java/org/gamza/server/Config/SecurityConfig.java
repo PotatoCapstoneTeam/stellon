298fa0e6bc155bd17a4c88034dde0871407907f5
JOE1534 <king01286@naver.com>
2022-11-21 17:30:38 +0900

    WebSocket Cors Fix #9

--- apps/server/src/main/java/org/gamza/server/Config/SecurityConfig.java
+++ apps/server/src/main/java/org/gamza/server/Config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
@@ -36,14 +37,13 @@ public class SecurityConfig {
     http
       .httpBasic().disable()
       .csrf().disable()
-      .cors()
+      .cors().configurationSource(corsConfigurationSource())
       .and()
 
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and()
 
       .authorizeRequests()
-      .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
       .antMatchers("/ws-stomp/**").permitAll()
       .antMatchers("/auth/login", "/auth/join", "/auth/reissue").permitAll()
       .anyRequest().authenticated()
@@ -67,14 +67,14 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOriginPatterns(Collections.singletonList("*")); // 모든 Origin에서의 요청을 허용
-    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 해당 Http Methods를 사용하는 요청을 허용
-    configuration.setAllowedHeaders(List.of("authorization", "refreshToken", "content-type", "x-auth-token")); // 해당 헤더를 사용하는 요청을 허용
-    configuration.setExposedHeaders(Collections.singletonList("x-auth-token")); // 헤더에 CSRF 토큰이 있는 요청에 대해 모든 응답 헤더를 노출
+    configuration.setAllowedOriginPatterns(Collections.singletonList("http://localhost:4200"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("authorization", "refreshToken", "content-type", "x-auth-token"));
+    configuration.setExposedHeaders(List.of("authorization", "refreshToken", "content-type", "x-auth-token"));
     configuration.setAllowCredentials(true); // 사용자 자격 증명(쿠키, 인증키) 사용을 허용할 것
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration); // 모든 URL에 대해 위의 설정을 사용해 CORS 처리를 할 것
+    source.registerCorsConfiguration("/**", configuration);
     return source;
   }
 }
