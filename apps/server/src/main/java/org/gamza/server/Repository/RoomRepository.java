eaddadb4eced286d827ba2e41d92346776d88835
JOE1534 <king01286@naver.com>
2022-11-29 01:08:39 +0900

    Fix MessageController #7 - Add fetch join for test

--- apps/server/src/main/java/org/gamza/server/Repository/RoomRepository.java
+++ apps/server/src/main/java/org/gamza/server/Repository/RoomRepository.java
@@ -2,10 +2,13 @@ package org.gamza.server.Repository;
 import org.gamza.server.Entity.GameRoom;
 import org.gamza.server.Enum.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<GameRoom, Long> {
   List<GameRoom> findGameRoomsByRoomType(RoomType roomType);
   GameRoom findGameRoomByRoomType(RoomType roomType);
+  @Query("select distinct r From GameRoom r join fetch r.players")
+  GameRoom findGameRoomById(Long id);
 }
