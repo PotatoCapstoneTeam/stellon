c10cc77112210ca6a4dc263c940a38bb98a205eb
JOE1534 <king01286@naver.com>
2022-11-29 02:04:22 +0900

    Fix MessageController #8 - fix fetch join to entity graph

--- apps/server/src/main/java/org/gamza/server/Repository/RoomRepository.java
+++ apps/server/src/main/java/org/gamza/server/Repository/RoomRepository.java
@@ -1,14 +1,17 @@
 package org.gamza.server.Repository;
 import org.gamza.server.Entity.GameRoom;
 import org.gamza.server.Enum.RoomType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<GameRoom, Long> {
   List<GameRoom> findGameRoomsByRoomType(RoomType roomType);
   GameRoom findGameRoomByRoomType(RoomType roomType);
-  @Query("select distinct r From GameRoom r join fetch r.players")
-  GameRoom findGameRoomById(Long id);
+
+  @Override
+  @EntityGraph(attributePaths = {"players"})
+  Optional<GameRoom> findById(Long id);
 }
