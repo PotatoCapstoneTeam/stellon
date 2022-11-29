958a8fc9c9890269057258e48978e3551233d99b
JOE1534 <king01286@naver.com>
2022-11-30 01:48:43 +0900

    Fix RestTemplate for START test #2 - Add header

--- apps/server/src/main/java/org/gamza/server/Controller/MessageController.java
+++ apps/server/src/main/java/org/gamza/server/Controller/MessageController.java
@@ -134,6 +134,8 @@ public class MessageController {
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+          " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 
         String url = "https://game.stellon.io/api";
         MultiValueMap<String, Object> response = new LinkedMultiValueMap<>();
