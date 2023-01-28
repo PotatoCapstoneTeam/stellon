6903f4045d8be1b61d211c6f8f32c1ced0764fff
JOE1534 <king01286@naver.com>
2023-01-29 00:38:49 +0900

    #150 [feat] 회원가입시 Record 초기화 추가 및 게임 결과 저장 서비스에 승패 추가

--- apps/server/src/main/java/org/gamza/server/Service/GameService.java
+++ apps/server/src/main/java/org/gamza/server/Service/GameService.java
@@ -21,16 +21,41 @@ public class GameService {
   private final UserRepository userRepository;
 
   @Transactional
+  public void initRecord(String nickname) {
+    User user = userRepository.findByNickname(nickname);
+    RecordResult result = RecordResult.builder()
+      .stageId("init")
+      .user(user)
+      .win(0).lose(0)
+      .kill(0).death(0)
+      .build();
+    resultRepository.save(result);
+  }
+
+  @Transactional
   public void gameResultSave(GameRequestDto gameRequestDto) {
     for (ResultRequestDto requestDto : gameRequestDto.getUsers()) {
       User user = userRepository.findById(requestDto.getUserId()).orElseThrow();
-      RecordResult recordResult = RecordResult.builder()
-        .kill(requestDto.getKill())
-        .death(requestDto.getDeath())
-        .user(user)
-        .stageId(gameRequestDto.getId())
-        .build();
-
+      RecordResult recordResult;
+      if (user.getTeamStatus() == gameRequestDto.getVictoryTeam()) {
+        recordResult = RecordResult.builder()
+          .kill(requestDto.getKill())
+          .death(requestDto.getDeath())
+          .win(1)
+          .lose(0)
+          .user(user)
+          .stageId(gameRequestDto.getId())
+          .build();
+      } else {
+        recordResult = RecordResult.builder()
+          .kill(requestDto.getKill())
+          .death(requestDto.getDeath())
+          .win(0)
+          .lose(1)
+          .user(user)
+          .stageId(gameRequestDto.getId())
+          .build();
+      }
       resultRepository.save(recordResult);
     }
   }
