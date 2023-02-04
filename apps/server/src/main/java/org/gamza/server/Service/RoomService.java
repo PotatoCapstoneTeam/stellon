8def84e22b76a70f2a7f52f81e1d62c287e65649
CJ-Park <king01286@naver.com>
2023-02-04 14:41:33 +0900

    #145 [fix] 방 나가기 에러 수정 - 3 - SQL Deadlock 에러

--- apps/server/src/main/java/org/gamza/server/Service/RoomService.java
+++ apps/server/src/main/java/org/gamza/server/Service/RoomService.java
@@ -151,10 +151,11 @@ public class RoomService {
   }
 
   @Transactional
-  public void removeUserToRoom(Long roomId, UserInfo userInfo) {
+  public GameRoom removeUserToRoom(Long roomId, UserInfo userInfo) {
     GameRoom room = roomRepository.findWithPlayersById(roomId).orElse(null);
     room.getPlayers().remove(userInfo.getPlayerNumber());
-    roomRepository.save(room);
+    userService.initStatus(userInfo.getUser().getNickname());
+    return room;
   }
 
   @Transactional
@@ -164,7 +165,6 @@ public class RoomService {
     User findUser = userRepository.findByEmail(jwtTokenProvider.parseClaims(token).getSubject());
     int index = getIndex(lobby.getPlayers(), findUser);
     lobby.getPlayers().remove(index);
-    roomRepository.save(lobby);
   }
 
   @Transactional
