4f41398d8560a88f8a4554cd5a09d5fa89255802
Pdom <pdom0327@gmail.com>
2023-04-27 10:09:07 +0900

    Refactor nexus hp

--- apps/game-server/src/entities/server-nexus.ts
+++ apps/game-server/src/entities/server-nexus.ts
@@ -1,8 +1,12 @@
 import { Nexus, NexusData, Scene } from '@stellon/game-core';
 import cuid from 'cuid';
 
+import { NEXUS_MAX_HP } from '../constant';
+
+export type ServerNexusData = Omit<NexusData, 'hp' | 'maxHp'>;
+
 export class ServerNexus extends Nexus {
-  constructor(scene: Scene, data: NexusData) {
-    super(cuid(), scene, data);
+  constructor(scene: Scene, data: ServerNexusData) {
+    super(cuid(), scene, { ...data, hp: NEXUS_MAX_HP, maxHp: NEXUS_MAX_HP });
   }
 }
--- libs/game-core/src/lib/entities/nexus.ts
+++ libs/game-core/src/lib/entities/nexus.ts
@@ -1,12 +1,15 @@
 import { Scene } from '../scenes';
 import { DamageableEntity, DamageableEntityData } from './entity';
 
-export type NexusData = Omit<DamageableEntityData, 'hp' | 'maxHp'>;
+export type NexusData = DamageableEntityData;
 
 export class Nexus extends DamageableEntity {
   constructor(id: string, scene: Scene, data: NexusData) {
     // ...data 앞은 기본값, 뒤는 고정값
-    super(id, scene, { name: '넥서스', ...data, hp: 2000, maxHp: 2000 });
+    super(id, scene, {
+      name: '넥서스',
+      ...data,
+    });
 
     scene.nexusGroup.add(this);
   }
